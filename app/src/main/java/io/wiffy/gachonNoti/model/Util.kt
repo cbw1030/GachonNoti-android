3f1e94ff2dcd35b303962392deea0816e3b89684
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-06-30 16:23:22 +0900

    20190630/4:23PM

--- app/src/main/java/io/wiffy/gachonNoti/model/Util.kt
+++ app/src/main/java/io/wiffy/gachonNoti/model/Util.kt
@@ -1,7 +1,10 @@
 package io.wiffy.gachonNoti.model
 
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
+import java.lang.Exception
+import java.text.SimpleDateFormat
 
 class Util {
     companion object {
@@ -34,6 +37,84 @@ class Util {
 
         @JvmStatic
         var novisible = false
+
+        @JvmStatic
+        fun timeToClass(hour: Int, minute: Int, noon: Boolean): ArrayList<String> {
+            // 오전 true 오후 false , hour minute
+            val myList = ArrayList<String>()
+
+            val absoluteMin = minute + 60 * if (noon) {
+                hour - 9
+            } else {
+                hour - 3
+            }
+
+            val relativeHour = absoluteMin / 60 + 1
+            val relativeMin = absoluteMin % 60
+
+            if (relativeHour <= 14 &&
+                relativeMin < 50
+            ) myList.add(relativeHour.toString())
+
+            when (absoluteMin) {
+                in 30 until 105 -> myList.add("A")
+                in 120 until 195 -> myList.add("B")
+                in 210 until 285 -> myList.add("C")
+                in 300 until 375 -> myList.add("D")
+                in 390 until 465 -> myList.add("E")
+                else -> {
+                }
+            }
+
+            return myList
+        }
+
+        @SuppressLint("SimpleDateFormat")
+        @JvmStatic
+        fun classToTime(time: String): LongArray {
+            val dt = SimpleDateFormat("HH:mm:ss")
+            var start: String
+            var end: String
+
+            when (time) {
+                "A" -> {
+                    start = "09:30:00"
+                    end = "10:45:00"
+                }
+                "B" -> {
+                    start = "11:00:00"
+                    end = "12:15:00"
+                }
+                "C" -> {
+                    start = "12:30:00"
+                    end = "13:45:00"
+                }
+                "D" -> {
+                    start = "14:00:00"
+                    end = "15:15:00"
+                }
+                "E" -> {
+                    start = "15:30:00"
+                    end = "16:45:00"
+                }
+                else -> {
+                    try {
+                        if (time.toInt() <= 14) {
+                            start = "${(time.toInt() + 9)}:00:00"
+                            end = "${(time.toInt() + 9)}:50:00"
+                        }else{
+                            start = "00:00:00"
+                            end = "00:00:00"
+                        }
+                    } catch (e: Exception) {
+                        start = "00:00:00"
+                        end = "00:00:00"
+                    }
+                }
+            }
+
+            return longArrayOf(dt.parse(start).time,dt.parse(end).time)
+        }
 
 
         const val mobileURL1 = "http://m.gachon.ac.kr/gachon/notice.jsp?pageNum="
