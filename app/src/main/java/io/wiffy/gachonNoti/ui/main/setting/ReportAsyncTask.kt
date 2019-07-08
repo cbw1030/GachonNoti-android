d480018818b3292d1b72dbb0cd6e6dc23ba3e4f7
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-07-08 18:56:27 +0900

    20190708/6:56PM

--- app/src/main/java/io/wiffy/gachonNoti/ui/main/setting/ReportAsyncTask.kt
+++ app/src/main/java/io/wiffy/gachonNoti/ui/main/setting/ReportAsyncTask.kt
@@ -2,18 +2,25 @@ package io.wiffy.gachonNoti.ui.main.setting
 
 import android.os.AsyncTask
 
-class ReportAsyncTask(private val myView: SettingContract.View, private val query: String):AsyncTask<Void,Void,Int>() {
+class ReportAsyncTask(private val myView: SettingContract.View, private val query: String) :
+    AsyncTask<Void, Void, Int>() {
     override fun onPreExecute() {
         myView.builderUp()
     }
-    override fun doInBackground(vararg params: Void?): Int {
 
-        return 0
-    }
+    override fun doInBackground(vararg params: Void?): Int =
+        try {
+
+            0
+        } catch (e: Exception) {
+
+            -1
+        }
+
 
     override fun onPostExecute(result: Int?) {
         myView.builderDismiss()
-        if(result==0)myView.makeToast("전송에 성공하였습니다.")
+        if (result == 0) myView.makeToast("전송에 성공하였습니다.")
         else myView.makeToast("전송에 실패하였습니다.")
     }
 }
