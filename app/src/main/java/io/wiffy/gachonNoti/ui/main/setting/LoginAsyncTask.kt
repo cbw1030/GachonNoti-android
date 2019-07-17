795bf24ec0bb51b1bb0af2a2a70acdd34c77363b
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-07-17 17:20:45 +0900

    20190717/5:20PM

--- app/src/main/java/io/wiffy/gachonNoti/ui/main/setting/LoginAsyncTask.kt
+++ app/src/main/java/io/wiffy/gachonNoti/ui/main/setting/LoginAsyncTask.kt
@@ -7,7 +7,13 @@ import android.os.Looper
 import io.wiffy.gachonNoti.model.StudentInformation
 import io.wiffy.gachonNoti.model.Util
 import io.wiffy.gachonNoti.ui.main.MainActivity
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
 
 class LoginAsyncTask(
     private val ids: String,
@@ -17,7 +23,7 @@ class LoginAsyncTask(
 ) : AsyncTask<Void, Void, Int>() {
 
     lateinit var studentInformation: StudentInformation
-    val ids_encoded =Util.getBase64Encode(ids)
+    val ids_encoded = Util.getBase64Encode(ids)
     val password_encoded = Util.getBase64Encode(password)
 
     override fun onPreExecute() {
@@ -29,7 +35,32 @@ class LoginAsyncTask(
     override fun doInBackground(vararg params: Void?): Int {
         if (!Util.isNetworkConnected(context)) return Util.ACTION_FAILURE
         try {
-//            TODO()
+            TODO()
+            val x =HashMap<String,String>()
+            x.put("user_id","okpsh0033")
+            x.put("password","Ok00330246!@")
+
+            val loginPageResponse =
+                Jsoup.connect("http://www.gachon.ac.kr/inc/_mobile_login_action.jsp")
+                    .timeout(3000)
+                    .header("Referer", "http://m.gachon.ac.kr/site/login.jsp")
+                    .header(
+                        "Accept",
+                        "image/gif,image/jpeg,image/pjpeg,application/x-ms-application,application/xaml+xml,application/x-ms-xbap,*/*"
+                    )
+                    .data(x)
+                    .header("Accept-Language", "ko-KR")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .header("Accept-Encoding", "gzip, deflate")
+                    .header("Host", "www.gachon.ac.kr")
+                    .header("Connection", "Keep-Alive")
+                    .userAgent("Mozilla")
+                    .method(Connection.Method.POST)
+                    .execute()
+
+            val loginTryCookie = loginPageResponse.cookies()
+            val loginPageDocument = loginPageResponse.parse()
+
             studentInformation = StudentInformation("박상현", "201735829", ids, password)
         } catch (e: Exception) {
             return Util.ACTION_FAILURE
