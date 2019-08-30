f4f1276bd6255b721d964a3e82572b63e536f54e
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-08-30 17:52:42 +0900

    20190830/5:52PM

--- app/src/main/java/io/wiffy/gachonNoti/ui/main/setting/administrator/AdministratorDialog.kt
+++ app/src/main/java/io/wiffy/gachonNoti/ui/main/setting/administrator/AdministratorDialog.kt
@@ -3,9 +3,11 @@ package io.wiffy.gachonNoti.ui.main.setting.administrator
 import android.content.Context
 import android.os.Bundle
 import io.wiffy.gachonNoti.R
+import io.wiffy.gachonNoti.`object`.Component
 import io.wiffy.gachonNoti.func.*
 import io.wiffy.gachonNoti.model.SuperContract
 import kotlinx.android.synthetic.main.dialog_administrator.*
+import java.util.*
 
 class AdministratorDialog(context: Context) : SuperContract.SuperDialog(context) {
 
@@ -21,7 +23,31 @@ class AdministratorDialog(context: Context) : SuperContract.SuperDialog(context)
         } else {
             option_female.isChecked = true
         }
-
+        if (getSharedItem("yearAuto", true)) {
+            option_year.setText(getSharedItem<String>("year"))
+            yearAuto.isChecked = false
+        } else {
+            option_year.setText(Component.YEAR)
+            yearAuto.isChecked = true
+        }
+        if (getSharedItem("semesterAuto", true)) {
+            option_autoSeme.isChecked = true
+        } else {
+            when (getSharedItem<Int>("semester")) {
+                1 -> option_1
+                2 -> option_2
+                3 -> option_summer
+                else -> option_winter
+            }.isChecked = true
+        }
+        yearAuto.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                option_year.setText(Calendar.getInstance().get(Calendar.YEAR).toString())
+                option_year.isClickable = false
+            } else {
+                option_year.isClickable = true
+            }
+        }
         lefts.run {
             setBackgroundResource(getThemeButtonResource())
             setOnClickListener {
@@ -52,6 +78,7 @@ class AdministratorDialog(context: Context) : SuperContract.SuperDialog(context)
     override fun onBackPressed() = dismiss()
 
     private fun setValue() {
+
         setSharedItems(
             Pair("number", option_number.text.toString().trim()),
             Pair("department", option_depart.text.toString().trim()),
@@ -62,7 +89,15 @@ class AdministratorDialog(context: Context) : SuperContract.SuperDialog(context)
                 "http://gcis.gachon.ac.kr/common/picture/haksa/shj/${option_number.text}.jpg"
             )
         )
-        setSharedItem("gender", option_male.isChecked)
+        setSharedItems(
+            Pair("gender", option_male.isChecked),
+            Pair("yearAuto", yearAuto.isChecked),
+            Pair("semesterAuto", option_autoSeme.isChecked)
+        )
+        setSharedItems(
+            Pair("year", option_year.text.toString().trim().toInt()),
+            Pair("semester", when ())
+        )
         dismiss()
         restartApp(context)
     }
