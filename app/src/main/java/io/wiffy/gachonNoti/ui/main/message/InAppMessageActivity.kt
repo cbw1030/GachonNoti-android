9f3df2f872a265f37325e25b4d376a7edfa9b6b0
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-09-21 17:47:56 +0900

    20190921/5:47PM

--- app/src/main/java/io/wiffy/gachonNoti/ui/main/message/InAppMessageActivity.kt
+++ app/src/main/java/io/wiffy/gachonNoti/ui/main/message/InAppMessageActivity.kt
@@ -1,15 +1,17 @@
 package io.wiffy.gachonNoti.ui.main.message
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import io.wiffy.gachonNoti.R
 import io.wiffy.gachonNoti.`object`.Component
 import io.wiffy.gachonNoti.function.setSharedItem
 import io.wiffy.gachonNoti.model.SuperContract
 import io.wiffy.gachonNoti.model.adapter.PagerAdapter
-import kotlinx.android.synthetic.main.dialog_inappmessage.*
+import kotlinx.android.synthetic.main.activity_inappmessage.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -20,7 +22,9 @@ class InAppMessageActivity : SuperContract.SuperActivity(), View.OnClickListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dialog_inappmessage)
+        setContentView(R.layout.activity_inappmessage)
+
+        setLayoutParams()
 
         mList = Component.mFragmentList ?: ArrayList()
 
@@ -47,6 +51,18 @@ class InAppMessageActivity : SuperContract.SuperActivity(), View.OnClickListener
             setSharedItem("message", false)
         }
         finish()
+    }
+
+    private fun setLayoutParams() {
+        try {
+            (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.let {
+                window.attributes.run {
+                    width = (it.width * 0.7).toInt()
+                    height = (it.height * 0.7).toInt()
+                }
+            }
+        } catch (e: Exception) {
+        }
     }
 
 }
--- app/src/main/java/io/wiffy/gachonNoti/ui/main/message/InAppMessageFragment.kt
+++ app/src/main/java/io/wiffy/gachonNoti/ui/main/message/InAppMessageFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.wiffy.gachonNoti.R
+import io.wiffy.gachonNoti.function.getRandomColorId
 import io.wiffy.gachonNoti.model.SuperContract
+import kotlinx.android.synthetic.main.fragment_inappmessage.view.*
 
 class InAppMessageFragment:SuperContract.SuperFragment() {
 
@@ -17,6 +19,10 @@ class InAppMessageFragment:SuperContract.SuperFragment() {
         savedInstanceState: Bundle?
     ): View? {
         myView = inflater.inflate(R.layout.fragment_inappmessage, container, false)
+
+        myView.inAppBackground.setBackgroundColor(resources.getColor(
+            getRandomColorId()
+        ))
 
         return myView
     }
